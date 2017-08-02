19d6401949fff25e0fa7fb50eec8639ff5fea6b9
wgillett <walter_gillett@hotmail.com>
2017-08-02 18:13:52 -0400

    line plot WIP

--- src/main/scala/com/cibo/evilplot/plot/LinePlot.scala
+++ src/main/scala/com/cibo/evilplot/plot/LinePlot.scala
@@ -3,36 +3,32 @@
  */
 package com.cibo.evilplot.plot
 
-import com.cibo.evilplot.geometry.{Drawable, Extent, Fit, FlipY, Point, Segment, WrapDrawable}
+import com.cibo.evilplot.geometry.{Drawable, Extent, Fit, FlipY, Point, Scale, Segment, WrapDrawable}
+import org.scalajs.dom.CanvasRenderingContext2D
 
 
 // TODO: use a layout manager to abstract out common logic
 class LinePlot(override val extent: Extent, data: Seq[Point], options: PlotOptions)
-  extends WrapDrawable {
+  extends Drawable {
 
   private val _drawable = {
-    val xvals = data.map(_.x)
-    val yvals = data.map(_.y)
-    val xMin = xvals.reduce(math.min)
-    val xMax = xvals.reduce(math.max)
-    val xBounds = Bounds(xMin, xMax)
-    val yMin = yvals.reduce(math.min)
-    val yMax = yvals.reduce(math.max)
-    val yBounds = Bounds(yMin, yMax)
-    val xAxisDrawBounds: Bounds = options.xAxisBounds.getOrElse(xBounds)
+    val xvals: Seq[Double] = data.map(_.x)
+    val yvals: Seq[Double] = data.map(_.y)
+    val xMin = xvals.reduce[Double](math.min)
+    val xMax = xvals.reduce[Double](math.max)
+    val yMin = yvals.reduce[Double](math.min)
+    val yMax = yvals.reduce[Double](math.max)
     val yAxisDrawBounds = options.yAxisBounds.getOrElse(Bounds(yMin, yMax))
 
-    // TODO: fix the XAxis and YAxis extents
     val xAxis = new XAxis(extent, xMin, xMax, options.numXTicks.getOrElse(4))
     val yAxis = new YAxis(extent, yAxisDrawBounds.min, yAxisDrawBounds.max, options.numYTicks.getOrElse(4))
 
+    val xscale = extent.width / (xMax - xMin)
+    val segment = Scale(xscale, xscale)(FlipY(Segment(data, strokeWidth = 0.1)))
+    val assembled = yAxis beside (xAxis below segment)
 
-
-    Fit(extent)(FlipY(Segment(data, strokeWidth = 0.1)))
+    Fit(extent)(assembled)
   }
 
-
-
-  def drawable: Drawable = _drawable
-
+  override def draw(canvas: CanvasRenderingContext2D) = _drawable.draw(canvas)
 }
