b4e03f0860cd7b832dc89d9fac23361020022f92
wgillett <walter_gillett@hotmail.com>
2017-08-04 21:38:08 -0400

    more fiddling

--- src/main/scala/com/cibo/evilplot/plot/LinePlot.scala
+++ src/main/scala/com/cibo/evilplot/plot/LinePlot.scala
@@ -10,7 +10,7 @@ import com.cibo.evilplot.layout.ChartLayout
 import com.cibo.evilplot.numeric.Ticks
 import org.scalajs.dom.CanvasRenderingContext2D
 
-
+/*
 // Plot paths connecting the points in data. Each Seq[Point] makes a path, drawn using the corresponding color.
 class LinePlot(override val extent: Extent, data: Seq[Seq[Point]], colors: Seq[Color], options: PlotOptions)
   extends Drawable {
@@ -55,6 +55,51 @@ class LinePlot(override val extent: Extent, data: Seq[Seq[Point]], colors: Seq[C
     }
     ChartLayout(extent, preferredSizeOfCenter = centerExtent, center = chartArea, left = yAxis, bottom = xAxis)
   }
+
+  override def draw(canvas: CanvasRenderingContext2D): Unit = layout.draw(canvas)
+
+}
+*/
+// Plot paths connecting the points in data. Each Seq[Point] makes a path, drawn using the corresponding color.
+class LinePlot(override val extent: Extent, data: Seq[Seq[Point]], colors: Seq[Color], options: PlotOptions)
+  extends Drawable {
+  require(data.length == colors.length)
+  val layout: Drawable = {
+    val xvals: Seq[Double] = data.flatMap(_.map(_.x))
+    val xMin: Double = xvals.reduce[Double](math.min)
+    val xMax: Double = xvals.reduce[Double](math.max)
+    val yvals: Seq[Double] = data.flatMap(_.map(_.y))
+    val yMin: Double = yvals.reduce[Double](math.min)
+    val yMax: Double = yvals.reduce[Double](math.max)
+    val xAxisDrawBounds: Bounds = options.xAxisBounds.getOrElse(Bounds(xMin, xMax))
+    val yAxisDrawBounds: Bounds = options.yAxisBounds.getOrElse(Bounds(yMin, yMax))
+    val xTicks = Ticks(xAxisDrawBounds, options.numXTicks.getOrElse(10))
+    val yTicks = Ticks(yAxisDrawBounds, options.numYTicks.getOrElse(10))
+    val xAxis = XAxis(xTicks)
+    val yAxis = YAxis(yTicks)
+    val centerFactor = 0.85   // proportion of the plot to allocate to the center
+    val centerExtent = extent * centerFactor
+    val xGridLines = Utils.maybeDrawable(options.xGridSpacing,
+      (xGridSpacing: Double) => VerticalGridLines(xTicks, xGridSpacing, color = White)(extent))
+    val yGridLines = Utils.maybeDrawable(options.yGridSpacing,
+      (yGridSpacing: Double) => HorizontalGridLines(yTicks, yGridSpacing, color = White)(extent))
+    val xScale = extent.width / xAxisDrawBounds.range
+    val yScale = extent.height / yAxisDrawBounds.range
+    val pathSeq: Seq[Drawable] = (data zip colors).map { case (_data: Seq[Point], color: Color) =>
+      FlipY(Scale(xScale, yScale)(StrokeStyle(color)(Path(_data, strokeWidth = 0.1))))
+    }
+    val paths = Group(pathSeq: _*)
+
+    //val scaledPaths = Scale(xScale, yScale)(groupedPaths)
+    val scaledPaths = paths
+    //val scaledPaths = FlipY(Scale(xScale, yScale)(groupedPaths transY yMax * yScale))
+    //val scaledPaths = groupedPaths transY yAxisDrawBounds.range * yScale
+//    Rect(extent) filled options.backgroundColor behind
+//      scaledPaths behind xGridLines behind yGridLines
+    yAxis(extent) above xAxis(extent)
+  }
+
+
 
   override def draw(canvas: CanvasRenderingContext2D): Unit = layout.draw(canvas)
 
