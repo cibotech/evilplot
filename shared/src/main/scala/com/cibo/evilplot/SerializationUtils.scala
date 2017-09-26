d455a52b0250c8fe5ab9e7091a72306829d4af22
Zach Graziano <zgraziano@cibotechnologies.com>
2017-09-26 12:44:44 -0400

    add decodeStr and encodeObj

--- shared/src/main/scala/com/cibo/evilplot/SerializationUtils.scala
+++ shared/src/main/scala/com/cibo/evilplot/SerializationUtils.scala
@@ -5,8 +5,10 @@
 package com.cibo.evilplot
 
 import com.cibo.evilplot.numeric.Histogram
-import io.circe.{Decoder, Encoder, HCursor, Json}
+import io.circe.{Decoder, Encoder, Error, HCursor, Json}
 import io.circe.generic.extras.Configuration
+import io.circe.syntax._
+import io.circe.parser.decode
 
 // This comes from Travis Brown's answer on serializing ADTs.
 // https://stackoverflow.com/questions/42165460/how-to-decode-an-adt-with-circe-without-disambiguating-objects
@@ -15,6 +17,22 @@ import io.circe.generic.extras.Configuration
 object SerializationUtils {
   implicit val withCaseClassConstructorName: Configuration = Configuration.default.withDiscriminator("what_am_i")
 
+  // These come from the  repo.
+
+  // Wrap the Circe JSON decode method and return just the desired type, not an Either.
+  // If parsing returns an error, throw the error rather than returning it.
+  def decodeStr[A: Decoder](input: String): A = {
+    val a: Either[Error, A] = decode[A](input)
+    a match {
+      case Left(error) => throw error
+      case Right(result) => result
+    }
+  }
+
+  // Encode the input object to JSON, then convert the JSON to a string and return it.
+  def encodeObj[A: Encoder](input: A): String = {
+    input.asJson.noSpaces
+  }
   // TODO: Fix the decoder.
 /*  implicit val encodeHistogram: Encoder[Histogram] = new Encoder[Histogram] {
     final def apply(h: Histogram): Json = Json.obj(
