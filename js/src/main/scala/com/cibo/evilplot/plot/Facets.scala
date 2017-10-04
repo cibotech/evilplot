f6864b76f54070809d31dc7071389dff4d36c32a
Zach Graziano <zgraziano@cibotechnologies.com>
2017-10-04 13:24:26 -0400

    Sizing of resulting plots fixed.

--- js/src/main/scala/com/cibo/evilplot/plot/Facets.scala
+++ js/src/main/scala/com/cibo/evilplot/plot/Facets.scala
@@ -4,15 +4,18 @@ import com.cibo.evilplot.geometry.{Drawable, Extent, Grid, WrapDrawable}
 import com.cibo.evilplot.interpreter.PlotDefinitionInterpreter
 import com.cibo.evilplot.plotdefs._
 
-
-
 /** Facets creates a 2-dimensional grid of plots extracted from a generic data object.
   * object into a subtype of PlotData.
   */
 
 class Facets(extent: Extent, facetsDef: FacetsDef) extends WrapDrawable {
-    private val interpreter = PlotDefinitionInterpreter
-    private val facets = facetsDef.plotDefs.map(interpreter.eval(_, None))
-    override def drawable: Drawable = new Grid(facetsDef.numRows, facetsDef.numCols,
-      facets, bottomPadding = 25, rightPadding = 25)
+  private val (numRows, numCols) = (facetsDef.numRows, facetsDef.numCols)
+  private val interpreter = PlotDefinitionInterpreter
+  private val bottomPadding = 30
+  private val rightPadding = 30
+  private val subPlotSize = Extent((extent.width - (numCols - 1) * rightPadding) / numCols,
+    (extent.height - (numRows - 1) * bottomPadding) / numRows)
+  private val facets = facetsDef.plotDefs.map(interpreter.eval(_, Some(subPlotSize)))
+  override def drawable: Drawable = new Grid(facetsDef.numRows, facetsDef.numCols,
+    facets, bottomPadding = bottomPadding, rightPadding = rightPadding)
 }
