ab8134bcafb7d04a870013e51ceb1efe9a5e75a4
Joe Wingbermuehle <joewing@joewing.net>
2018-01-30 16:28:54 -0600

    Measure text in Java.

--- jvm/src/main/scala/com/cibo/evilplot/geometry/TextMetrics.scala
+++ jvm/src/main/scala/com/cibo/evilplot/geometry/TextMetrics.scala
@@ -4,6 +4,10 @@ object TextMetrics extends TextMetricsInterface {
   def measure(text: Text): Extent = {
     val canvas = new java.awt.Canvas()
     val graphics = canvas.getGraphics
-    ???
+    val font = graphics.getFont.deriveFont(text.size.toFloat)
+    val metrics = graphics.getFontMetrics(font)
+    val width = metrics.stringWidth(text.msg)
+    val height = metrics.getHeight
+    Extent(width, height)
   }
 }
