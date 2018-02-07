41de8369bb2cbb5438c8149602a77e5d11997047
Joe Wingbermuehle <joewing@joewing.net>
2018-02-07 09:05:16 -0600

    Add Heatmap.

--- shared/src/main/scala/com/cibo/evilplot/plot/renderers/BarRenderer.scala
+++ shared/src/main/scala/com/cibo/evilplot/plot/renderers/BarRenderer.scala
@@ -1,6 +1,6 @@
 package com.cibo.evilplot.plot.renderers
 
-import com.cibo.evilplot.colors.{Color, DefaultColors}
+import com.cibo.evilplot.colors.{Color, DefaultColors, ScaledColorBar}
 import com.cibo.evilplot.geometry._
 import com.cibo.evilplot.plot.Bar
 
@@ -26,5 +26,25 @@ object BarRenderer {
   def default(
     color: Color = DefaultColors.barColor
   ): BarRenderer = stackedRenderer(Seq(color))
+
+  def temperature(
+    colors: Seq[Color] = Color.stream
+  )(
+    bars: Seq[Seq[Bar]]
+  ): BarRenderer = {
+    val minValue = bars.flatten.minBy(_.height).height
+    val maxValue = bars.flatten.maxBy(_.height).height
+    val colorCount = bars.flatten.map(_.height).distinct.size
+    temperature(ScaledColorBar(colors.take(colorCount), minValue, maxValue))
+  }
+
+  def temperature(
+    colorBar: ScaledColorBar
+  ): BarRenderer = new BarRenderer {
+    def render(bar: Bar, barExtent: Extent, index: Int): Drawable = {
+      val color = colorBar.getColor(bar.height)
+      Rect(barExtent.width, barExtent.height).filled(color)
+    }
+  }
 
 }
