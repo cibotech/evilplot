a3a87fee8582ff62dbf1a7003c7ec41ccba1ac42
Zach Graziano <zgraziano@cibotechnologies.com>
2018-02-08 08:18:42 -0600

    begin adding scaled

--- shared/src/main/scala/com/cibo/evilplot/plot/renderers/SurfaceRenderer.scala
+++ shared/src/main/scala/com/cibo/evilplot/plot/renderers/SurfaceRenderer.scala
@@ -1,21 +1,30 @@
 package com.cibo.evilplot.plot.renderers
 
-import com.cibo.evilplot.colors.{Color, DefaultColors}
+import com.cibo.evilplot.colors.{Color, DefaultColors, ScaledColorBar}
 import com.cibo.evilplot.geometry.{Drawable, Path, StrokeStyle}
-import com.cibo.evilplot.numeric.{GridData, MarchingSquares, Point, Point3}
+import com.cibo.evilplot.numeric.{Point, Point3}
 
 trait SurfaceRenderer {
-  def render(points: Seq[Point3]): Drawable
+  def render(points: Seq[Seq[Point3]]): Drawable
 }
 
 object SurfaceRenderer {
   private val defaultStrokeWidth: Double = 2
   def contours(strokeWidth: Double = defaultStrokeWidth,
                color: Color = DefaultColors.pathColor): SurfaceRenderer = new SurfaceRenderer {
-    def render(points: Seq[Point3]): Drawable = {
-      points.grouped(2)
+    def render(points: Seq[Seq[Point3]]): Drawable = {
+      points.map(_.grouped(2)
         .map(seg => StrokeStyle(Path(seg.map(p => Point(p.x, p.y)), strokeWidth), color))
-        .toSeq.group
+        .toSeq.group).group
     }
   }
+
+  def densityColorContours(strokeWidth: Double = defaultStrokeWidth): SurfaceRenderer = new SurfaceRenderer {
+    def render(points: Seq[Seq[Point3]]): Drawable = {
+      val bar = ScaledColorBar(DefaultColors)
+    }
+  }
+
+  def desityColorContours(strokeWidth: Double = defaultStrokeWidth,
+                          bar: ScaledColorBar): SurfaceRenderer = ???
 }
