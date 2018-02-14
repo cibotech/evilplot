7100ba9a5a693bf9226cdf77df317684e3d0e3b3
Joe Wingbermuehle <joewing@joewing.net>
2018-02-14 13:25:29 -0600

    Don't pad the left and right of line plots.

--- shared/src/main/scala/com/cibo/evilplot/plot/XyPlot.scala
+++ shared/src/main/scala/com/cibo/evilplot/plot/XyPlot.scala
@@ -30,21 +30,24 @@ object XyPlot {
 
   /** Create an XY plot (ScatterPlot, LinePlot are both special cases) from some data.
     *
-    * @param data          The points to plot.
-    * @param pointRenderer A function to create a Drawable for each point to plot.
-    * @param pathRenderer A function to create a Drawable for all the points (such as a path)
-    * @param boundBuffer   Extra padding to add to bounds as a fraction.
+    * @param data           The points to plot.
+    * @param pointRenderer  A function to create a Drawable for each point to plot.
+    * @param pathRenderer   A function to create a Drawable for all the points (such as a path)
+    * @param xboundBuffer   Extra padding to add to bounds as a fraction.
+    * @param yboundBuffer   Extra padding to add to bounds as a fraction.
     * @return A Plot representing an XY plot.
     */
   def apply(
-             data: Seq[Point],
-             pointRenderer: PointRenderer = PointRenderer.default(),
-             pathRenderer: PathRenderer = PathRenderer.default(),
-             boundBuffer: Double = defaultBoundBuffer
-           ): Plot = {
-    require(boundBuffer >= 0.0)
-    val xbounds = Plot.expandBounds(Bounds(data.minBy(_.x).x, data.maxBy(_.x).x), boundBuffer)
-    val ybounds = Plot.expandBounds(Bounds(data.minBy(_.y).y, data.maxBy(_.y).y), boundBuffer)
+    data: Seq[Point],
+    pointRenderer: PointRenderer = PointRenderer.default(),
+    pathRenderer: PathRenderer = PathRenderer.default(),
+    xboundBuffer: Double = defaultBoundBuffer,
+    yboundBuffer: Double = defaultBoundBuffer
+  ): Plot = {
+    require(xboundBuffer >= 0.0)
+    require(yboundBuffer >= 0.0)
+    val xbounds = Plot.expandBounds(Bounds(data.minBy(_.x).x, data.maxBy(_.x).x), xboundBuffer)
+    val ybounds = Plot.expandBounds(Bounds(data.minBy(_.y).y, data.maxBy(_.y).y), yboundBuffer)
     val legends = pointRenderer.legendContext.toSeq ++ pathRenderer.legendContext.toSeq
     Plot(xbounds, ybounds, XyPlotRenderer(data, pointRenderer, pathRenderer), legendContext = legends)
   }
