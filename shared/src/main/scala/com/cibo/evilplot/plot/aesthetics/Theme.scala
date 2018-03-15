451a7459e7d5bbeb45ae67a435bdfaef454e83ff
Joe Wingbermuehle <joewing@joewing.net>
2018-03-15 14:48:02 -0500

    Add implicitNotFound.

--- shared/src/main/scala/com/cibo/evilplot/plot/aesthetics/Theme.scala
+++ shared/src/main/scala/com/cibo/evilplot/plot/aesthetics/Theme.scala
@@ -1,5 +1,8 @@
 package com.cibo.evilplot.plot.aesthetics
 
+import scala.annotation.implicitNotFound
+
+@implicitNotFound("No implicit Theme found. You may wish to import com.cibo.evilplot.aesthetics.DefaultTheme._")
 trait Theme {
   val fonts: Fonts
   val colors: Colors
