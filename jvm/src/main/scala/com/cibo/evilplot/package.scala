7cb777316fd2bef5634649bfa0789dddb5e4bb57
Zachary Graziano <zachary.a.graziano@gmail.com>
2018-03-30 17:38:26 -0400

    Improve resolution of pngs.

--- jvm/src/main/scala/com/cibo/evilplot/package.scala
+++ jvm/src/main/scala/com/cibo/evilplot/package.scala
@@ -3,20 +3,27 @@ package com.cibo
 import java.awt.image.BufferedImage
 
 import com.cibo.evilplot.geometry._
+import javax.imageio.ImageIO
 package object evilplot {
   implicit class AwtDrawableOps(r: Drawable) {
     /** Return a BufferedImage containing the contents of this Drawable. */
     def asBufferedImage: BufferedImage = {
+      val scale = 2
       val paddingHack = 20
-      val bi = new BufferedImage(r.extent.width.toInt,
-                                 r.extent.height.toInt,
-                                 BufferedImage.TYPE_INT_ARGB)
+      val bi = new BufferedImage(r.extent.width.toInt * scale,
+        r.extent.height.toInt * scale,
+        BufferedImage.TYPE_INT_ARGB)
       val gfx = bi.createGraphics()
+      gfx.scale(2.0, 2.0)
       val padded = r.padAll(paddingHack / 2)
-
       fit(padded, r.extent).draw(Graphics2DRenderContext(gfx))
       gfx.dispose()
       bi
+    }
+
+    /** Write a Drawable to a file as a PNG. */
+    def write(file: java.io.File): Unit = {
+      ImageIO.write(asBufferedImage, "png", file)
     }
   }
 }
