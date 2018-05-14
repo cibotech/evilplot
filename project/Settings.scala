fbc017db4f8e51a878dd579398ef6eed5ed9d736
Zachary Graziano <zgraziano@cibotechnologies.com>
2018-05-14 18:00:56 -0400

    Bump ScalaCheck

--- project/Settings.scala
+++ project/Settings.scala
@@ -18,7 +18,7 @@ object Settings {
     val scalactic = "3.0.1"
     val scopt = "3.5.0"
     val circe = "0.9.0"
-    val scalacheck = "1.13.5"
+    val scalacheck = "1.14.0"
   }
 
   val sharedDependencies = Def.setting(
