9899cfa6d7cc25c6291814f4b0b4afb8fdaa9914
Tristrum Tuttle <ttuttle@cibotechnologies.com>
2018-07-03 12:47:46 -0400

    initial prototype

--- jvm/src/main/scala/com/cibo/evilplot/displayPlot.scala
+++ jvm/src/main/scala/com/cibo/evilplot/displayPlot.scala
@@ -1,17 +1,14 @@
 package com.cibo.evilplot
 
-import java.awt.{Color, EventQueue, Graphics, Graphics2D}
+import java.awt.{Graphics, Graphics2D}
 
-import com.cibo.evilplot.numeric.Point
-import com.cibo.evilplot.plot.{FunctionPlot, Overlay, Plot, ScatterPlot}
-import javax.swing.JPanel
-import javax.swing.JFrame
-import java.awt.event.{ActionEvent, KeyEvent}
+import com.cibo.evilplot.plot.{Plot}
+import javax.swing.{JFrame, JOptionPane, JPanel}
+import java.awt.event.{ActionEvent, ComponentAdapter, ComponentEvent}
 import java.io.File
 
-import com.cibo.evilplot.colors.HTMLNamedColors
+import com.cibo.evilplot.demo.DemoPlots.{theme}
 import com.cibo.evilplot.geometry.{Drawable, Extent}
-import com.cibo.evilplot.plot.aesthetics.DefaultTheme._
 
 object displayPlot {
 
@@ -26,7 +23,7 @@ object displayPlot {
       super.paintComponent(g)
       val g2 = g.asInstanceOf[Graphics2D]
       if (!(drawable == None)) {
-        g2.drawImage(drawable.get.asBufferedImage, 0, 0, this)
+        g2.drawImage(drawable.get.asBufferedImage, -40, 0, this)
 
       }
     }
@@ -34,69 +31,83 @@ object displayPlot {
 
   object DrawableFrame extends JFrame {
 
-    import javax.swing.JMenu
     import javax.swing.JMenuBar
     import javax.swing.JMenuItem
 
     var drawable: Option[Drawable] = None
-    var plotTitle: String = "anonymous_plot"
+    var plot: Option[Plot] = None
+
 
     private def createMenuBar(): Unit = {
       val menubar = new JMenuBar
-      val quit = new JMenuItem("Exit")
-      quit.addActionListener((event: ActionEvent) => {
-        def quit(event: ActionEvent) = {
-          System.exit(0)
-        }
-        quit(event)
-      })
       val save = new JMenuItem("Save")
       save.addActionListener((event: ActionEvent) => {
         def save(event: ActionEvent) = {
-          drawable.get.write(new File(s"/tmp/$plotTitle.png"))
+          val result = JOptionPane.showInputDialog("Enter a filename:")
+          savePlot(result)
         }
         save(event)
       })
-//      menubar.add(quit)
       menubar.add(save)
       setJMenuBar(menubar)
     }
 
     private def init(): Unit = {
-      setTitle(plotTitle)
+      setTitle("Plot")
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
-      setSize(drawable.get.extent.width.toInt*4, drawable.get.extent.height.toInt*4 + 20)
-      DrawablePanel.setDrawable(drawable.get)
+      if (!drawable.isEmpty) {
+        setSize(drawable.get.extent.width.toInt*2, drawable.get.extent.height.toInt*2 + 20)
+        DrawablePanel.setDrawable(drawable.get.scaled(0.5,0.5))
+      }
+      else {
+        setSize(400, 420)
+        DrawablePanel.setDrawable(plot.get.render(new Extent(200, 200)).scaled(0.5, 0.5))
+      }
       add(DrawablePanel)
       createMenuBar()
+      addComponentListener(new ComponentAdapter {
+        override def componentResized(e: ComponentEvent): Unit = {
+          resizePlot(DrawableFrame.getWidth, DrawableFrame.getHeight)
+        }
+      })
       setVisible(true)
     }
 
-    def apply(drawnPlot: Drawable): Unit = {
-      drawable = Some(drawnPlot)
+    def getPlotExtent: Extent = {
+      Extent(this.getWidth/2, (this.getHeight-20)/2)
+    }
+
+    def resizePlot(width: Int, height: Int): Unit = {
+      if (!plot.isEmpty) {
+        DrawablePanel.setDrawable(plot.get.render(getPlotExtent).scaled(0.5,0.5))
+      }
+    }
+
+    def savePlot(result: String): Unit = {
+      if(!plot.isEmpty) {
+        plot.get.render(getPlotExtent).scaled(0.5,0.5).write(new File(s"/tmp/$result.png"))
+      } else {
+        drawable.get.write(new File(s"/tmp/$result.png"))
+      }
+    }
+
+    def apply(drawnPlot: Option[Drawable], notDrawnPlot: Option[Plot]): Unit = {
+      drawable = drawnPlot
+      plot = notDrawnPlot
       init()
     }
 
-    def apply(drawnPlot: Drawable, withTitle: String): Unit = {
-      drawable = Some(drawnPlot)
-      plotTitle = withTitle
-      init()
-    }
   }
 
+  def apply(plot: Plot): Unit = {
+    JFrame.setDefaultLookAndFeelDecorated(true)
+    DrawableFrame(None, Some(plot))
+  }
 
   def apply(drawnPlot: Drawable): Unit = {
     JFrame.setDefaultLookAndFeelDecorated(true)
-    DrawableFrame(drawnPlot)
+    DrawableFrame(Some(drawnPlot), None)
   }
 
 }
 
-object testRunner extends App {
-  val item =
-    Overlay(
-      ScatterPlot(Seq(Point(1,1), Point(2, 2), Point(3, 3))),
-      FunctionPlot.series(i => i*i, "hey", HTMLNamedColors.dodgerBlue).standard()
-    )
-  displayPlot(item.render(Extent(400, 400)).scaled(0.5, 0.5))
-}
