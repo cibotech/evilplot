acdea4b2421e02f457144d740c2f95febd3c159f
Elijah Forbes-Summers <eforbes-summers@cibotechnologies.com>
2018-07-10 11:39:41 -0400

    stuff

--- shared/src/main/scala/com/cibo/evilplot/demo/DemoPlots.scala
+++ shared/src/main/scala/com/cibo/evilplot/demo/DemoPlots.scala
@@ -439,13 +439,16 @@ object DemoPlots {
       .xAxis()
       //.yBoundsAxis(plot.ybounds, Position.Left)
       .boundsFnAxis(_ => Bounds(0, 4000000), Position.Left)
+      .yAxis(position = Position.Right)
       //.yBoundsAxis(Bounds(0, 100), Position.Right)
       //.xBoundsAxis(plot.xbounds, Position.Bottom)
       //.xBoundsAxis(Bounds(0, 90000000), Position.Top)
-      .boundsFnAxis(_ => Bounds(0, 90), Position.Top)
+      //.boundsFnAxis(_ => Bounds(0, 90), Position.Top)
+      .xAxis(position = Position.Top)
       //.xBoundsAxis(Bounds(1000000, 50000000), Position.Top)
       //.xbounds(0, 30)
-      .xbounds(0, 100)
+      //.xbounds(0, 100)
+      .boundsFnAxis(_ => Bounds(10, 40), Position.Overlay) //XXX kinda useless without range scaling
       .render(plotAreaSize)
   }
 
