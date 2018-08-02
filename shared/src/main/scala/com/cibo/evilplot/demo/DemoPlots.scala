2e72e4ce8097a26302e88697384db9923c1ee0d2
Tristrum Tuttle <ttuttle@cibotechnologies.com>
2018-08-02 16:27:38 -0400

    add path renderer with coloring

--- shared/src/main/scala/com/cibo/evilplot/demo/DemoPlots.scala
+++ shared/src/main/scala/com/cibo/evilplot/demo/DemoPlots.scala
@@ -304,17 +304,22 @@ object DemoPlots {
       .map(_.toDouble)
       .zip(
         Seq(
-          0.0, 0.1, 0.0, 0.1, 0.0, 0.1
+          0.0, 0.1, 0.2, 0.1, 0.0, 0.1
         ))
       .map(Point.tupled)
 
+    val pathRenderer = Some(
+      PathRenderer.depthColor(
+        Seq(0, 1, 2, 1, 2, 0),
+        Some(ContinuousColoring.gradient(HTMLNamedColors.dodgerBlue, HTMLNamedColors.crimson))))
+
     LinePlot(
-      data
+      data,
+      pathRenderer = pathRenderer
     ).ybounds(0, .12)
-      .yAxis()
-      .xGrid()
-      .yGrid()
-      .frame()
+      .standard()
+      .ybounds(0, 0.2)
+      .rightLegend()
       .render(plotAreaSize)
   }
 
@@ -327,7 +332,6 @@ object DemoPlots {
 
     Heatmap(data).title("Heatmap Demo").xAxis().yAxis().rightLegend().render(plotAreaSize)
   }
-
 
   lazy val facetedPlot: Drawable = {
     val years = 2007 to 2013
