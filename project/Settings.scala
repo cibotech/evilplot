afdb11f13d640db3176662241c99dc975b29d9d0
Erik Erlandson <eerlands@redhat.com>
2018-09-14 13:45:30 -0700

    Adds evilplot-jupyter-scala package for displaying drawable and plot objects directly into jupyter-scala output cells

--- project/Settings.scala
+++ project/Settings.scala
@@ -19,6 +19,7 @@ object Settings {
     val scalactic = "3.0.1"
     val scopt = "3.5.0"
     val circe = "0.9.0"
+    val jupyterScala = "0.4.1"
     val scalacheck = "1.14.0"
   }
 
@@ -32,6 +33,11 @@ object Settings {
       "org.scalacheck" %%% "scalacheck" % versions.scalacheck % "test",
       "org.scalatest" %%% "scalatest" % versions.scalaTest % "it,test",
       compilerPlugin("org.scalamacros" %% "paradise" % "2.1.0" cross CrossVersion.full)
+    ))
+
+  val jupyterScalaDependencies = Def.setting(
+    Seq(
+      "org.jupyter-scala" %% "kernel-api" % versions.jupyterScala % Provided
     ))
 
   val jvmDependencies = Def.setting(
