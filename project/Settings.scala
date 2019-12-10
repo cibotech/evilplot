4b742238cb315dff59bd4015d96fb0a76844a794
Brian Schlining <bschlining@gmail.com>
2019-12-10 13:37:43 -0800

    Re-added `%%%` syntax

--- project/Settings.scala
+++ project/Settings.scala
@@ -31,44 +31,45 @@ object Settings {
       "io.circe" %%% "circe-generic" % versions.circe,
       "io.circe" %%% "circe-parser" % versions.circe,
       "io.circe" %%% "circe-generic-extras" % versions.circe,
-      "org.scalactic" %% "scalactic" % versions.scalactic,
-      "org.scalacheck" %% "scalacheck" % versions.scalacheck % "test",
-      "org.scalatest" %% "scalatest" % versions.scalaTest % "it,test",
-//      compilerPlugin("org.scalamacros" %% "paradise" % "2.1.1" cross CrossVersion.full)
-    ))
+      "org.scalactic" %%% "scalactic" % versions.scalactic,
+      "org.scalacheck" %%% "scalacheck" % versions.scalacheck % "test",
+      "org.scalatest" %%% "scalatest" % versions.scalaTest % "it,test"
+    )
+  )
 
   val jupyterScalaDependencies = Def.setting(
     Seq(
       "org.jupyter-scala" %% "kernel-api" % versions.jupyterScala % Provided
-    ))
-
-
-
-  val sharedMathDependencies = Def.setting(
-    Seq(
-      "org.scalactic" %% "scalactic" % versions.scalactic,
-      "org.scalatest" %% "scalatest" % versions.scalaTest % "test",
-//      compilerPlugin("org.scalamacros" %% "paradise" % "2.1.1" cross CrossVersion.Full)
     )
   )
 
+  val sharedMathDependencies = Def.setting(
+    Seq(
+      "org.scalactic" %%% "scalactic" % versions.scalactic,
+      "org.scalatest" %%% "scalatest" % versions.scalaTest % "test"
+    )
+  )
 
   val jvmMathDependencies = Def.setting(
     Seq(
-      "org.scalanlp"               %% "breeze"          % "1.0",
-      "org.scalanlp"               %% "breeze-natives"  % "1.0", // removing will still work but will be slower
-    ))
+      "org.scalanlp" %% "breeze" % "1.0",
+      "org.scalanlp" %% "breeze-natives" % "1.0" // removing will still work but will be slower
+    )
+  )
 
   val jvmDependencies = Def.setting(
     Seq(
-      ))
+      )
+  )
 
   val scalajsDependencies = Def.setting(
     Seq(
       "org.scala-js" %%% "scalajs-dom" % versions.scalaDom
-    ))
+    )
+  )
 
   val jsDependencies = Def.setting(
     Seq(
-      ))
+      )
+  )
 }
